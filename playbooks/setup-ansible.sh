5629f25dbf017889b4548885fe804f4c3b503467
Juni Inacio <24542013+juniinacio@users.noreply.github.com>
2023-07-07 13:23:45 +0200

    Initial commit for hello event (#1)
    * Initial commit
    
    * Add rulebooks directory
    
    * Update requirements
    
    * Add initial rulebook
    
    ---------
    
    Co-authored-by: Juni Inacio <Juni.Inacio@macaw.nl>

---
+++ playbooks/setup-ansible.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_PATH="${BASH_SOURCE}"
+while [ -L "${SCRIPT_PATH}" ]; do
+  SCRIPT_DIR="$(cd -P "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+  SCRIPT_PATH="$(readlink "${SCRIPT_PATH}")"
+  [[ ${SCRIPT_PATH} != /* ]] && SCRIPT_PATH="${SCRIPT_DIR}/${SCRIPT_PATH}"
+done
+SCRIPT_PATH="$(readlink -f "${SCRIPT_PATH}")"
+SCRIPT_DIR="$(cd -P "$(dirname -- "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+
+read -s -p "Ansible user password: " ansible_user_password
+echo ""
+ansible-playbook ${SCRIPT_DIR}/setup-ansible.yml --ask-pass --extra-vars ansible_user_password=${ansible_user_password}
